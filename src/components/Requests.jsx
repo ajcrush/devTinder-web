bcee1487b9e1af059f74e394c92141e1eb86974d
Mohit sharma <mohitanand8987@gmail.com>
2025-01-08 21:43:38 +0530

    implement accepted and rejected facility in our application

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,12 +1,22 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constant";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequest } from "../utils/requestSlice";
+import { addRequest, removeRequest } from "../utils/requestSlice";
 import { useEffect } from "react";
 
 const Requests = () => {
   const requests = useSelector((store) => store.request);
   const dispatch = useDispatch();
+  const reviewRequests = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {}
+  };
   const fetchRequest = async () => {
     try {
       const res = await axios.get(BASE_URL + "/user/request/received", {
@@ -21,7 +31,7 @@ const Requests = () => {
     fetchRequest();
   }, []);
   if (!requests || requests.length === 0) {
-    return <h1>No connection found</h1>;
+    return <h1 className="flex justify-center my-10">No connection found</h1>;
   }
 
   return (
@@ -50,8 +60,18 @@ const Requests = () => {
               {about && <p>{about}</p>}
             </div>
             <div>
-              <button className="btn btn-primary mx-4">Accept</button>
-              <button className="btn btn-secondary">Reject</button>
+              <button
+                className="btn btn-primary mx-4"
+                onClick={() => reviewRequests("accepted", connection._id)}
+              >
+                Accept
+              </button>
+              <button
+                className="btn btn-secondary"
+                onClick={() => reviewRequests("rejected", connection._id)}
+              >
+                Reject
+              </button>
             </div>
           </div>
         );
